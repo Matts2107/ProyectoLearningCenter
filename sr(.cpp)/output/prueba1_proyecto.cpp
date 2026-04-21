ea42045df953fd785cbbff3f5d9fac7feccfb05d
madriansiguenza <madriansiguenza@gmail.com>
2026-04-20 21:08:01 -0500

    Update prueba1_proyecto.cpp

--- sr(.cpp)/output/prueba1_proyecto.cpp
+++ sr(.cpp)/output/prueba1_proyecto.cpp
@@ -18,8 +18,8 @@ public:
     
     void mostrarMateria() const {
         std::cout << "ID: " << codigoM 
-                << " | Nombre: " << nombreM 
-                << " | Multiplicador: " << multiplicador << "\n";
+                  << " | Nombre: " << nombreM 
+                  << " | Multiplicador: " << multiplicador << "\n";
     }
     
     std::string getMateria() const { return nombreM; }
@@ -70,9 +70,9 @@ public:
     // Polimorfismo: Implementamos el método abstracto
     void mostrarPerfil() const override {
         std::cout << "ID: " << id_banner 
-                << " | Nombre: " << nombre 
-                << " | Correo: " << correo 
-                << " | Tarifa: $" << tarifa_hora << "/h\n";
+                  << " | Nombre: " << nombre 
+                  << " | Correo: " << correo 
+                  << " | Tarifa: $" << tarifa_hora << "/h\n";
     }
     double getTarifa() const { 
         return tarifa_hora; 
@@ -88,7 +88,7 @@ public:
 class SistemaLearningCenter {
 private:
     // Almacenamiento polimórfico de usuarios
-    std::vector<std::shared_ptr<Persona>> usuarios; 
+    std::vector<std::shared_ptr<Persona>> usuarios;
     std::vector<std::shared_ptr<Materia>> materias;
 public:
     SistemaLearningCenter() {}
@@ -99,7 +99,8 @@ public:
         usuarios.push_back(std::make_shared<Tutor>("Ana Lopez", "00124", "ana@u.edu", 18.5));
         materias.push_back(std::make_shared<Materia>("Calculo Diferencial", "MATH101", 2));
         materias.push_back(std::make_shared<Materia>("Fisica", "PHYS101", 3));
-        std::cout << "-> Datos de prueba cargados  exitosamente.\n";
+        
+        std::cout << "-> Datos de prueba cargados exitosamente.\n";
     }
 
     // Método para el Administrador (Create Tutor)
@@ -214,7 +215,7 @@ public:
             if (m->getCodigo() == codigoBuscar) {
                 m->setMultiplicador(nuevoMultiplicador);
                 std::cout << "\n[EXITO] El multiplicador de la materia " << m->getMateria() 
-                        << " se actualizo a " << nuevoMultiplicador << ".\n";
+                          << " se actualizo a " << nuevoMultiplicador << ".\n";
                 return;
             }
         }
@@ -254,7 +255,7 @@ int main() {
         
         if (opcionPrincipal == 1) {
             std::cout << "\n[Entrando al Modulo de Estudiante... (Proximamente)]\n";
-            
+
             menuEstudiante(sistema);
         } 
         else if (opcionPrincipal == 2) {
@@ -354,8 +355,6 @@ void menuAdministrador(SistemaLearningCenter& sistema) {
         }
     } while (opcion != 10);
 }
-
-
 void menuEstudiante(SistemaLearningCenter& sistema) {
     int opcion;
     std::string nom, id, cor, carr, idTut, idMat, fecha;
